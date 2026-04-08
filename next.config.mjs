614154165cc6d0c9b49cbebb9498792e3002f8d1
Sammed Ghattad <sammedghattad2132002711@gmail.com>
2026-04-08 20:44:45 +0530

    fix: restore unoptimized image setting in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
+		unoptimized: true,
 		remotePatterns: [
 			{
 				protocol: "https",
@@ -8,6 +9,5 @@ const nextConfig = {
 			},
 		],
 	},
-};
 
 export default nextConfig;
